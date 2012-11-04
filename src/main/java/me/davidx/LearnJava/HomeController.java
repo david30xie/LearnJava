6e5098184fbef11832dd520a327488f2a468a082
David Xie <david.scriptfan@gmail.com>
2012-11-04 23:58:13 +0800

    init

---
+++ src/main/java/me/davidx/LearnJava/HomeController.java
@@ -0,0 +1,12 @@
+package me.davidx.LearnJava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/")
+    public String home() {
+        return "WEB-INF/home.jsp";
+    }
+}
